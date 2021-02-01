26ed78cf8a9ab878b21c70feabfa6a80ed23d8df
Andreas Lindner <42932900+AndreasULindner@users.noreply.github.com>
2021-02-01 15:44:30 +0000

    Update run_demo.R
    Simplified environment by removing ggplot and reshape2

--- run_demo.R
+++ run_demo.R
@@ -1,10 +1,8 @@
 # library(data.table)
-library(reshape2)
-library(ggplot2)
 library(deSolve)
 
 pop.foxRabbits.ini <- c(rabbits = 34.9134, foxes = 3.8566)
-kinetics.foxRabbits <- c(0.4807, 0.0248, 0.0.0276, 0.9271)
+kinetics.foxRabbits <- c(0.4807, 0.0248, 0.0276, 0.9271)
 t.duration <- seq(0, 100, 1/12)
 
 model.lv <- function (t, Y, v) {  
@@ -19,15 +17,12 @@ solution.foxRabbits <- ode(y = pop.foxRabbits.ini,
                            times = t.duration, 
                            parms = kinetics.foxRabbits) 
 
-# change the result from wide to long format using melt(...)
-solution.foxRabbits.long <- melt(as.data.frame(solution.foxRabbits), id = c("time"))
-
 # Draw a figure
-ggplot(solution.foxRabbits.long, aes(time, value)) + 
-  geom_line(aes(colour = variable, linetype = variable)) + 
-  theme_classic() +
-  labs(caption = "Lotka Voltera - Rabbits versus Foxes",
-       x = "time [a.U.]",
-       y = "number of animals",
-       colour = "Species") + 
-  theme(legend.position = "top")
+
+plot(solution.foxRabbits[, "time"], solution.foxRabbits[, "rabbits"], 
+     xlab = "time [years]", ylab = "number of animals [thousands]",
+     type="l", col="#23689b", ylim = c(-0, 90), lwd = 2)
+
+lines(solution.foxRabbits[, "time"], solution.foxRabbits[, "foxes"], col="#ec4646", lwd = 2, lty = 4)
+
+legend("bottom", c("Rabbits","Foxes"), fill=c("#23689b","#ec4646") ) 
